8ba6d571dd18eb2d0f3facf35e84851455d28336
emanuele5180 <macina5180>
2020-07-28 02:35:09 +0200

    ajax error

--- main.js
+++ main.js
@@ -4,20 +4,38 @@
 
 function submitInfo() {
   var btn = $ ('#submit');
-  btn.click(giveInformation);
+  btn.click(giveInformation); //attivazione del bottone e recupero del valore della searchbar con la funzione
 
 
 
-  console.log();
+
 }
 
 function giveInformation(info){
-  var info = $('#searchbar').val();
-  
+  var info = $('#searchbar').val(); //valore della searchbar
+  $(".details").text('');
+
   console.log(info);
+  $.ajax ({
+    url:'https://api.themoviedb.org/3/movie/550?api_key=8ac24a7a81a1a4b07020a1a1e2059da8',
+    method:'GET',
+    success: function(data) {
+      var success = data ['success'];
+      var value = data ['response'];
+
+      console.log(data);
 
 
-}
+      // console.log(accaBi);
+
+
+    },
+    error: function(errori) {
+      console.log('errore');
+    }
+
+
+})
 
 function init() {
   console.log("helloworld");
